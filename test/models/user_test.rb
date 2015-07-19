22f62cd91e86fb839460e1c2e5e4d5911e7c0a90
Andrei Klepets <testisl18@gmail.com>
2015-07-19 18:08:02 +0000

    Excersizes chapter 6

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -25,6 +25,15 @@ class UserTest < ActiveSupport::TestCase
     assert_not @user.valid?
   end
   
+  test "email validation should reject invalid addresses" do
+    invalid_addresses = %w[user@example,com user_at_foo.org user.name@example.
+                           foo@bar_baz.com foo@bar+baz.com foo@bar..com]
+    invalid_addresses.each do |invalid_address|
+      @user.email = invalid_address
+      assert_not @user.valid?, "#{invalid_address.inspect} should be invalid"
+    end
+  end
+  
   test "email should not be too long" do
     @user.email = "a" * 244 + "@example.com"
     assert_not @user.valid?
@@ -35,6 +44,12 @@ class UserTest < ActiveSupport::TestCase
     duplicate_user.email = @user.email.upcase
     @user.save
     assert_not duplicate_user.valid?
+  end
+  
+  test "email should be downcased" do
+    @user.email = "Foo@ExAMPle.CoM"
+    @user.save
+    assert_equal @user.email.downcase, @user.reload.email
   end
   
   test "password should be present(non blank)" do
